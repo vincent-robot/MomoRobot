3d3fe1341dbcb382c07413237397a1bd89d600c1
vincent-robot <vincent.hornain@gmail.com>
2021-12-19 19:16:22 +0100

    ajout structure Ros

---
+++ ros2/my_robot_tutorials/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'my_robot_tutorials'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='ubuntu@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+       'console_scripts': [
+        'minimal_python_node = my_robot_tutorials.my_python_node:main'
+        ],
+    },
+)
